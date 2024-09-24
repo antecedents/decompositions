bfc0604f04e364ca3f1d03fc0bbd731eda6c7ae9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-24 23:13:07 +0100

    Add config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+Module config.py
+"""
+
+
+class Config:
+    """
+    Description
+    -----------
+
+    A class for configurations
+    """
+
+    def __init__(self) -> None:
+        
+        self.seed = 5
