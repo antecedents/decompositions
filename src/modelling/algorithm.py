e11e11ad801eb9b05ea500afbd894b74182b92f7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-07 23:09:04 +0000

    Update src/modelling/algorithm.py

--- src/modelling/algorithm.py
+++ src/modelling/algorithm.py
@@ -1,5 +1,6 @@
 import logging
 import pandas as pd
+import numpy as np
 
 class Algorithm:
 
@@ -11,9 +12,11 @@ class Algorithm:
 
         self.__data = data
 
-    def exc(self, group: str):
+    def exc(self, n_lags: int, n_equations: int, group: str):
         """
 
+        :param n_lags:
+        :param n_equations:
         :param group:
         :return:
         """
@@ -21,3 +24,10 @@ class Algorithm:
         columns = [column for column in self.__data.columns if column != group]
         logging.info(columns)
 
+        coordinates = {'lags': np.arange(n_lags),
+                       'equations': columns,
+                       'cv': columns}
+        logging.info(coordinates)
+
+        groups = self.__data[group].unique()
+        logging.info(groups)
