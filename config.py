32c7692dfaf564af68769bbde7f3e814ac972f4d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-07 21:10:23 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 """
 Module config.py
 """
+import os
 
 
 class Config:
@@ -12,5 +13,14 @@ class Config:
     """
 
     def __init__(self) -> None:
+        """
+        Constructor
+        """
+
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.data_ = os.path.join(os.getcwd(), 'data')
         
         self.seed = 5
+
+        # Configuration files
+        self.s3_parameters_key = 's3_parameters.yaml'
