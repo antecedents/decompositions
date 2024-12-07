9fb2ea8aae669bd28b702a65df04fc0d2860fad3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-07 22:40:00 +0000

    Update src/modelling/interface.py: In Progress

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -1,6 +1,8 @@
 """Module interface.py"""
+import logging
 import pandas as pd
 
+import src.modelling.algorithm
 import src.modelling.temporary
 
 
@@ -10,13 +12,17 @@ class Interface:
     """
 
     def __init__(self):
-        pass
 
-    @staticmethod
-    def exc():
+        self.__fields = ['hospital_code', 'n_attendances']
+
+    def exc(self):
         """
 
         :return:
         """
 
         temporary: pd.DataFrame = src.modelling.temporary.Temporary().exc()
+        temporary.sort_values(by=['health_board_code', 'hospital_code', 'week_ending_date'], ascending=True, inplace=True)
+        logging.info(temporary)
+
+        src.modelling.algorithm.Algorithm(data=temporary[self.__fields]).exc(group='hospital_code')
