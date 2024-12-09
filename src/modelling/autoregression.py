130b76cfca1a4652113f3bc9c54ab8896490948c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-09 14:52:53 +0000

    Update src/modelling/autoregression.py

--- src/modelling/autoregression.py
+++ src/modelling/autoregression.py
@@ -1,5 +1,7 @@
+"""Module autoregression.py"""
 import logging
 import pymc
+
 
 class Autoregression:
     """
@@ -8,11 +10,16 @@ class Autoregression:
     """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
+
+        self.__logger = logging.getLogger(__name__)
 
     @staticmethod
     def exc(lag_coefficients, n_equations, n_lags, blob):
