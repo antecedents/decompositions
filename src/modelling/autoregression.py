413f4ed0a658e4826458f4c2e3d43f22c0bd4f8f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-09 14:51:14 +0000

    Add src/modelling/autoregression.py

---
+++ src/modelling/autoregression.py
@@ -0,0 +1,40 @@
+import logging
+import pymc
+
+class Autoregression:
+    """
+    A helper that constructs the autoregressive step for the marginal contribution of each lagged
+    term, of each time series equation.
+    """
+
+    def __init__(self):
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+    @staticmethod
+    def exc(lag_coefficients, n_equations, n_lags, blob):
+        """
+
+        :param lag_coefficients: pymc.Normal
+        :param n_equations: np.int
+        :param n_lags: np.int
+        :param blob: pandas.DataFrame
+        :return:
+        """
+
+        steps = []
+        for j in range(n_equations):
+            ar = pymc.math.sum(
+                [
+                    pymc.math.sum(lag_coefficients[j, i] * blob.values[n_lags - (i + 1) : -(i + 1)], axis=-1)
+                    for i in range(n_lags)
+                ],
+                axis=0,
+            )
+            steps.append(ar)
+        beta = pymc.math.stack(steps, axis=-1)
+
+        return beta
