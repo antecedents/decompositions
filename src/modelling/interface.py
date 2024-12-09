15b4336fab336cbf8a835420670e927b2edf354a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-09 16:12:08 +0000

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -15,19 +15,38 @@ class Interface:
 
         self.__fields = ['hospital_code', 'n_attendances']
 
+        # Temporarily, read-in the local copy of the Amazon data set.
+        self.__temporary: pd.DataFrame = src.modelling.temporary.Temporary().exc()
+
+        # How many hospitals per board?
+        self.__frequencies = self.__temporary.copy()[['health_board_code', 'hospital_code']].drop_duplicates().value_counts(
+            subset='health_board_code', ascending=False)
+
+    def __strategy(self, health_board_code: str):
+        """
+        Either:<br>
+        A Bayesian hierarchical model
+             <ul><li>Per health board</li>
+             <li>Covering all boards</li></ul>
+
+        :param health_board_code:
+        :return:
+        """
+
+        return self.__temporary.copy().loc[
+            self.__temporary['health_board_code'] == health_board_code, self.__fields]
+
     def exc(self):
         """
 
         :return:
         """
 
-        # Temporarily, read-in the local copy of the Amazon data set.
-        temporary: pd.DataFrame = src.modelling.temporary.Temporary().exc()
-
         # Initially, focus on the health board that has the largest number of hospitals.
-        frequencies = temporary[['health_board_code', 'hospital_code']].drop_duplicates().value_counts(subset='health_board_code', ascending=False)
-        logging.info(frequencies)
-        logging.info(frequencies.index.name)
-        logging.info(frequencies.index[0])
+        logging.info(self.__frequencies.index.name)
+        logging.info(self.__frequencies.index[0])
+        data = self.__strategy(health_board_code=self.__frequencies.index[0])
 
-        # src.modelling.algorithm.Algorithm(data=temporary[self.__fields]).exc(n_lags=2, n_equations=3, group='hospital_code')
+        # Hence
+        src.modelling.algorithm.Algorithm(data=data).exc(
+            n_lags=2, n_equations=3, group='hospital_code')
