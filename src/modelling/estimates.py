1a3861c32b692c49942e0fb2252626c98d24c05e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-31 22:51:56 +0000

    Update src/modelling/estimates.py: In Progress

---
+++ src/modelling/estimates.py
@@ -0,0 +1,26 @@
+import pymc
+
+import config
+
+class Estimates:
+
+    def __init__(self, n_timings: int, ahead: int):
+        """
+
+        :param n_timings:
+        :param ahead:
+        """
+
+        self.__n_timings = n_timings
+        self.__prediction_length = self.__n_timings + ahead
+
+        # Configurations
+        self.__configurations = config.Config()
+
+    def exc(self, ARMODEL: pymc.model.core.Model):
+
+        with ARMODEL:
+
+            ARMODEL.add_coords({'obs_id_ext': range(
+                self.__n_timings - self.__configurations.seasons, self.__prediction_length, 1)})
+            ARMODEL.add_coords({'obs_id_original': range(self.__n_timings, self.__prediction_length, 1)})
