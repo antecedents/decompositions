aa17370f42a6d1fa3aab246c14481576de9d8adf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-13 21:52:47 +0000

    Update src/data/features.py: In Progress

--- src/data/features.py
+++ src/data/features.py
@@ -1,6 +1,20 @@
+import pandas as pd
+
 
 class Features:
 
-    def __init__(self):
+    def __init__(self, data: pd.DataFrame):
+        """
+
+        :param data:
+        """
+
+        self.__frame = data.copy()
+
+    def __data(self):
         pass
+
+    def exc(self):
+
+        computations = []
 
