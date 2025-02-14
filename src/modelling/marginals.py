c32dcf445c16c8912b0592202bc62b8e9af3353d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-14 22:54:08 +0000

    Update src/modelling/marginals.py: In Progress

---
+++ src/modelling/marginals.py
@@ -0,0 +1,29 @@
+
+import pymc
+import pytensor
+
+class Marginals:
+    """
+    Computing auto regressive steps
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def exc(lag_coefs, n_eqs, n_lags, df):
+
+        ars = []
+        for j in range(n_eqs):
+            ar = pymc.math.sum(
+                [
+                    pymc.math.sum(lag_coefs[j, i] * df.values[n_lags - (i + 1) : -(i + 1)], axis=-1)
+                    for i in range(n_lags)
+                ],
+                axis=0,
+            )
+            print(ar)
+            ars.append(ar)
+        beta = pymc.math.stack(ars, axis=-1)
+
+        return beta
