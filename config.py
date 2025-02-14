c0585e37a15d91a2ee8ed77f983d9c96f0b1c2ed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-14 23:43:22 +0000

    Update config.py

--- config.py
+++ config.py
@@ -20,17 +20,23 @@ class Config:
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
         self.artefacts_ = os.path.join(self.warehouse, 'artefacts')
 
-        # Cut-off, boundary, for modelling; additionally the number of final points to exclude.
-        self.boundary = '2020-06-01'
-
         # Seed
         self.seed = 5
 
         # Configuration files
         self.s3_parameters_key = 's3_parameters.yaml'
 
-        # For architecture JSON
+        '''
+        For architecture JSON
+        '''
+
+        # Fields
         self.fields = ['week_ending_date', 'health_board_code', 'hospital_code',  'n_attendances']
+
+        # Seasons, trends, forecast weeks ahead, etc.
         self.seasons = 52
         self.trends = 1
         self.ahead = 9
+
+        # Cut-off, boundary, for modelling; additionally the number of final points to exclude.
+        self.boundary = '2020-06-01'
