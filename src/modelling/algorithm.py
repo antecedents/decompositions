e417cc53dc711e6c3fdd443592e89c8b70b5ee08
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-14 22:22:44 +0000

    Update src/modelling/algorithm.py: In Progress

--- src/modelling/algorithm.py
+++ src/modelling/algorithm.py
@@ -9,83 +9,70 @@ class Algorithm:
     The multi-level algorithm
     """
 
-    def __init__(self, frames: pd.DataFrame, n_timings: int) -> None:
-        """
+    def __init__(self) -> None:
+        pass
 
-        :param frames:
-        :param n_timings:
-        """
-
-        self.__frames = frames
-        self.__groups = self.__frames['treatment_location'].unique()
-
-        # Or, the original time points
-        self.__timings = list(range(n_timings))
-
-        # ...
-        self.__priors = {
-            'coefficients': {'size': 53},
-            'sigma': 8,
-            'init': {'mu': 0.04, 'sigma': 0.1, 'size': 52},
-        }
-
-    def exc(self):
+    @staticmethod
+    def exc(n_lags, n_eqs, df, group_field, prior_checks=True):
         """
 
         :return:
         """
 
-        # Initialise the model
-        with pymc.Model() as ARMODEL_:
-            pass
+        cols = [col for col in df.columns if col != group_field]
+        coords = {"lags": np.arange(n_lags) + 1,
+                  "equations": cols
+                  }
 
-        # Mutable by default
-        ARMODEL_.add_coord('obs_id', self.__timings)
+        groups = df[group_field].unique()
 
-        with ARMODEL_:
+        priors = {
+            "noise": {"sigma": 1}
+        }
 
-            # The hierarchical priors
-            _c_location = pymc.Normal('_c_location', 0, 0.1)
-            _c_scale = pymc.InverseGamma('_c_scale', 3, 0.5)
+        with pymc.Model(coords=coords) as model:
 
-            # Time points
-            points = pymc.Data('points', self.__timings, dims='obs_id')
+            ## Hierarchical Priors
+            alpha_hat_location = pymc.Normal("alpha_hat_location", 0, 0.1)
+            alpha_hat_scale = pymc.InverseGamma("alpha_hat_scale", 3, 0.5)
+            beta_hat_location = pymc.Normal("beta_hat_location", 0, 0.1)
+            beta_hat_scale = pymc.InverseGamma("beta_hat_scale", 3, 0.5)
 
-            # By group
-            for group in self.__groups:
 
-                core = self.__frames.copy().loc[self.__frames['treatment_location'] == group, :]
-                core.sort_index(ascending=True, ignore_index=False, inplace=True)
-                __seq = core['n_difference'].to_numpy()
+            for grp in groups:
 
-                # And
-                _z_scale = pymc.InverseGamma(f'_z_scale_{group}', 3, 0.5)
-
-                # The data containers
-                observations = pymc.Data(f'observations_{group}', __seq, dims='obs_id')
-
-                # Setting priors for each coefficient in the AR process
-                coefficients = pymc.Normal(f'coefficients_{group}', mu=_c_location, sigma=(_c_scale*_z_scale),
-                                           size=self.__priors['coefficients']['size'])
-                sigma = pymc.HalfNormal(f'sigma_{group}', self.__priors['sigma'])
-
-                # Initialisation per ...
-                init = pymc.Normal.dist(
-                    self.__priors['init']['mu'], self.__priors['init']['sigma'], size=self.__priors['init']['size']
+                df_grp = df[df[group_field] == grp][cols]
+                z_scale_beta = pymc.InverseGamma(f"z_scale_beta_{grp}", 3, 0.5)
+                z_scale_alpha = pymc.InverseGamma(f"z_scale_alpha_{grp}", 3, 0.5)
+                lag_coefs = pymc.Normal(
+                    f"lag_coefs_{grp}",
+                    mu=beta_hat_location,
+                    sigma=beta_hat_scale * z_scale_beta,
+                    dims=["equations", "lags"],
+                )
+                alpha = pymc.Normal(
+                    f"alpha_{grp}",
+                    mu=alpha_hat_location,
+                    sigma=alpha_hat_scale * z_scale_alpha,
+                    dims=("equations",)
                 )
 
-                # Autoregressive process with p lags; lags = self.__priors['coef']['size'] - 1
-                process = pymc.AR(
-                    f'ar_{group}', coefficients, sigma=sigma, init_dist=init, constant=True,
-                    steps=points.eval().shape[0] - (self.__priors['coefficients']['size'] - 1),
-                    dims='obs_id')
+                betaX = calc_ar_step(lag_coefs, n_eqs, n_lags, df_grp)
+                betaX = pymc.Deterministic(f"betaX_{grp}", betaX)
+                mean = alpha + betaX
 
-                # Likelihood
-                pymc.Normal(f'likelihood_{group}', mu=process, sigma=sigma, observed=observations, dims='obs_id')
+                ## This is an alternative likelihood that can recover sensible estimates of the coefficients
+                ## But lacks the multivariate correlation between the timeseries.
+                sigma = pymc.HalfNormal(f'sigma_{grp}', sigma=priors["noise"]["sigma"], dims=["equations"])
+                pymc.Normal(f'likelihood_{grp}', mu=mean, sigma=sigma, observed=df_grp.values[n_lags:])
 
-            # Sampling
-            details_ = pymc.sample_prior_predictive()
-            details_.extend(pymc.sample(2000, random_seed=100, target_accept=0.95))
-            details_.extend(pymc.sample_posterior_predictive(details_))
 
-        return details_, ARMODEL_
+            if prior_checks:
+                idata = pymc.sample_prior_predictive()
+                return model, idata
+            else:
+                idata = pymc.sample_prior_predictive()
+                idata.extend(pymc.sampling.jax.sample_blackjax_nuts(2000, random_seed=100))
+                idata.extend(pymc.sample_posterior_predictive(idata))
+
+        return model, idata
