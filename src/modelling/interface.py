81ba87d045cd2e0d2ba9b07844a7adea282f9962
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-18 21:51:41 +0000

    Update src/modelling/interface.py: In Progress

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -56,4 +56,4 @@ class Interface:
             data = self.__get_data(board=board)
             model, idata = self.__algorithms.exc(
                 n_lags=self.__configurations.n_lags, n_eqs=self.__configurations.n_eqs,
-                df=data, group_field=self.__configurations.group_field)
+                data=data, group_field=self.__configurations.group_field)
