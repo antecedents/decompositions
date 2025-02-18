1c0e6676f6439714f28ef45873a4fbe7233e6ca1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-18 22:40:12 +0000

    Update src/modelling/algorithm.py: In Progress

--- src/modelling/algorithm.py
+++ src/modelling/algorithm.py
@@ -24,7 +24,7 @@ class Algorithm:
         self.__configurations = config.Config()
         self.__marginals = src.modelling.marginals.Marginals()
 
-    def exc(self, n_lags: int, n_equations: int, data: pd.DataFrame, group_field: str, prior_checks: bool = False) -> (
+    def exc(self, n_lags: int, n_equations: int, data: pd.DataFrame, leaves: str, prior_checks: bool = False) -> (
             typing.Tuple)[pymc.model.core.Model, arviz.data.InferenceData]:
         """
 
@@ -32,16 +32,16 @@ class Algorithm:
         :param n_equations: <br># of independent variables.  Beware, this algorithm is inappropriate for cases whereby
                       there are two or more variates because it does not consider multi-variate covariance.<br>
         :param data: <br>The training data.<br>
-        :param group_field: <br>The field that identifies an instance's group<br>
+        :param leaves: <br>The field that identifies an instance's group<br>
         :param prior_checks: <br>Focusing on priors only?<br>
         :return:
         """
 
-        cols = [col for col in data.columns if col != group_field]
+        cols = [col for col in data.columns if col != leaves]
         coords = {'lags': np.arange(n_lags) + 1,
                   'equations': cols}
 
-        groups = data[group_field].unique()
+        groups = data[leaves].unique()
 
         with pymc.Model(coords=coords) as model:
 
@@ -52,32 +52,32 @@ class Algorithm:
             beta_hat_scale = pymc.InverseGamma('beta_hat_scale', alpha=3, beta=0.5)
 
             # By institution
-            for grp in groups:
+            for group in groups:
 
-                segment = data[data[group_field] == grp][cols]
-                z_scale_beta = pymc.InverseGamma(f'z_scale_beta_{grp}', alpha=3, beta=0.5)
-                z_scale_alpha = pymc.InverseGamma(f'z_scale_alpha_{grp}', alpha=3, beta=0.5)
+                segment = data[data[leaves] == group][cols]
+                z_scale_beta = pymc.InverseGamma(f'z_scale_beta_{group}', alpha=3, beta=0.5)
+                z_scale_alpha = pymc.InverseGamma(f'z_scale_alpha_{group}', alpha=3, beta=0.5)
                 lc = pymc.Normal(
-                    f'lc_{grp}',
+                    f'lc_{group}',
                     mu=beta_hat_location,
                     sigma=beta_hat_scale * z_scale_beta,
                     dims=['equations', 'lags'],
                 )
                 alpha = pymc.Normal(
-                    f'alpha_{grp}',
+                    f'alpha_{group}',
                     mu=alpha_hat_location,
                     sigma=alpha_hat_scale * z_scale_alpha,
                     dims=('equations',)
                 )
 
                 beta_x = self.__marginals.exc(lc=lc, n_equations=n_equations, n_lags=n_lags, segment=segment)
-                beta_x = pymc.Deterministic(f'beta_x_{grp}', beta_x)
+                beta_x = pymc.Deterministic(f'beta_x_{group}', beta_x)
                 mean = alpha + beta_x
 
                 # Likelihood
-                sigma = pymc.HalfNormal(f'sigma_{grp}',
+                sigma = pymc.HalfNormal(f'sigma_{group}',
                                         sigma=self.__configurations.priors['noise']['sigma'], dims=['equations'])
-                pymc.Normal(f'likelihood_{grp}', mu=mean, sigma=sigma, observed=segment.values[n_lags:])
+                pymc.Normal(f'likelihood_{group}', mu=mean, sigma=sigma, observed=segment.values[n_lags:])
 
             if prior_checks:
                 idata = pymc.sample_prior_predictive(random_seed=self.__configurations.seed)
