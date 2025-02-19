abd6b3bc50a582a6e85d50c62b3ad017519a8a8a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-19 14:59:08 +0000

    Add src/extraneous/interface.py: In Progress

---
+++ src/extraneous/interface.py
@@ -0,0 +1,11 @@
+import arviz
+
+import pandas as pd
+
+class Interface:
+
+    def __init__(self):
+        pass
+
+    def exc(self, data: pd.DataFrame, idata: arviz.data.InferenceData):
+        pass
