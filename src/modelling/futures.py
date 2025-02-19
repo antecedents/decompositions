a71b1e86b524189d8bcf6fe186c2bddf6b57f50f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-19 13:06:43 +0000

    Update src/modelling/futures.py: In Progress

--- src/modelling/futures.py
+++ src/modelling/futures.py
@@ -1,5 +1,6 @@
 """Module futures.py"""
-import pymc
+
+import numpy as np
 
 import config
 
@@ -8,23 +9,46 @@ class Futures:
     For estimating future values
     """
 
-    def __init__(self, n_timings: int, ahead: int):
+    def __init__(self, points: np.ndarray):
         """
 
-        :param n_timings:
-        :param ahead:
+        :param points:
         """
 
-        self.__n_timings = n_timings
-        self.__prediction_length = self.__n_timings + ahead
+        self.__points = points
 
         # Configurations
         self.__configurations = config.Config()
+        self.__rng = np.random.default_rng(self.__configurations.seed)
 
-    def exc(self, ARMODEL: pymc.model.core.Model):
+    def _forecast(self, intercept, lc, noise, forecast):
+        """
 
-        with ARMODEL:
+        :param intercept: constant coefficient
+        :param lc: lag coefficients
+        :param noise:
+        :param forecast: # of forecast points
+        :return:
+        """
 
-            ARMODEL.add_coords({'obs_id_ext': range(
-                self.__n_timings - self.__configurations.seasons, self.__prediction_length, 1)})
-            ARMODEL.add_coords({'obs_id_original': range(self.__n_timings, self.__prediction_length, 1)})
+        n_points = len(self.__points)
+
+        # The underlying structure of the draws is → the shape of the training data variable + # of forecasts
+        draws = np.zeros((self.__points.shape[0] + forecast, self.__points.shape[1]))
+
+        # Fill the new array with the observed data
+        draws[:n_points] = self.__points[:]
+
+        # Drawing
+        for i in range(forecast):
+            ar_ = np.sum(lc[:, 0] * draws[(n_points + i - self.__configurations.n_lags):(n_points + i)])
+            mean = intercept + ar_
+            draws[n_points + i] = self.__rng.normal(mean, noise)
+
+        # Replace all observed data with NaN
+        draws[:-forecast - 1] = np.nan
+
+        return draws
+
+    def exc(self):
+        pass
