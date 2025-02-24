fb60085cd117ca847db150fc63a1c0fb7f43e6b7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-24 20:08:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -23,49 +23,11 @@ class Config:
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
         self.artefacts_ = os.path.join(self.warehouse, 'artefacts')
 
-        # Seed
-        self.seed = 5
-
         # Configuration files
         self.s3_parameters_key = 's3_parameters.yaml'
 
-        '''
-        For architecture JSON
-        '''
-
         # Fields
-        self.fields = ['week_ending_date', 'health_board_code', 'hospital_code',  'n_attendances']
-
-        # Seasons, trends, forecast weeks ahead, etc.
-        self.seasons = 52
-        self.trends = 1
-        self.ahead = 9
-
-        # The number of
-        #   lags: via partial auto-correlation function of a series that has undergone (a) ln, (b) season
-        #         differencing, (c) trend differencing.
-        #   independent variables: In this context, `n_attendances` only
-        # And more.
-        self.n_lags = 52
-        self.n_equations = 1
-        self.leaves = 'hospital_code'
+        self.fields = ['week_ending_date', 'health_board_code', 'hospital_code', 'n_attendances']
 
         # Cut-off, boundary.
         self.boundary = '2020-06-01'
-
-        # Priors
-        self.priors = {
-            'alpha_hat_location': {'mu': 0, 'sigma': 0.1},
-            'alpha_hat_scale': {'alpha': 3, 'beta': 0.5},
-            'beta_hat_location': {'mu': 0, 'sigma': 0.1},
-            'beta_hat_scale': {'alpha': 3, 'beta': 0.5},
-            'z_scale_alpha': {'alpha': 3, 'beta': 0.5},
-            'z_scale_beta': {'alpha': 3, 'beta': 0.5},
-            'noise': {'sigma': 1}
-        }
-
-        # The # of samples for predicting.
-        self.n_samples = 500
-
-        # Error interval
-        self.interval = [45, 55]
