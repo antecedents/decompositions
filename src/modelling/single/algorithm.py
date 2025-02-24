9ac7af550ee7477d8df8fd2b40181944fe523bb4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-24 22:35:16 +0000

    Update src/modelling/single/algorithm.py: In Progress

--- src/modelling/single/algorithm.py
+++ src/modelling/single/algorithm.py
@@ -1,11 +1,10 @@
 """Module algorithm.py"""
 import typing
+
 import numpy as np
 import pandas as pd
 import pymc
 import pymc.sampling.jax
-
-import arviz.data
 
 import config
 import src.modelling.single.marginals
@@ -24,8 +23,7 @@ class Algorithm:
         self.__configurations = config.Config()
         self.__marginals = src.modelling.single.marginals.Marginals()
 
-    def exc(self, n_lags: int, n_equations: int, data: pd.DataFrame, leaves: str, prior_checks: bool = False) -> (
-            typing.Tuple)[pymc.model.core.Model, arviz.data.InferenceData]:
+    def exc(self, n_lags: int, n_equations: int, data: pd.DataFrame, leaves: str, prior_checks: bool = False):
         """
 
         :param n_lags: <br># of non-constant coefficients<br>
@@ -45,47 +43,4 @@ class Algorithm:
 
         with pymc.Model(coords=coords) as model:
 
-            # Hierarchical Priors
-            alpha_hat_location = pymc.Normal('alpha_hat_location', mu=0, sigma=0.1)
-            alpha_hat_scale = pymc.InverseGamma('alpha_hat_scale', alpha=3, beta=0.5)
-            beta_hat_location = pymc.Normal('beta_hat_location', mu=0, sigma=0.1)
-            beta_hat_scale = pymc.InverseGamma('beta_hat_scale', alpha=3, beta=0.5)
-
-            # By institution
-            for group in groups:
-
-                segment = data[data[leaves] == group][cols]
-                z_scale_beta = pymc.InverseGamma(f'z_scale_beta_{group}', alpha=3, beta=0.5)
-                z_scale_alpha = pymc.InverseGamma(f'z_scale_alpha_{group}', alpha=3, beta=0.5)
-                lc = pymc.Normal(
-                    f'lc_{group}',
-                    mu=beta_hat_location,
-                    sigma=beta_hat_scale * z_scale_beta,
-                    dims=['equations', 'lags'],
-                )
-                alpha = pymc.Normal(
-                    f'alpha_{group}',
-                    mu=alpha_hat_location,
-                    sigma=alpha_hat_scale * z_scale_alpha,
-                    dims=('equations',)
-                )
-
-                beta_x = self.__marginals.exc(lc=lc, n_equations=n_equations, n_lags=n_lags, segment=segment)
-                beta_x = pymc.Deterministic(f'beta_x_{group}', beta_x)
-                mean = alpha + beta_x
-
-                # Likelihood
-                sigma = pymc.HalfNormal(f'sigma_{group}',
-                                        sigma=self.__configurations.priors['noise']['sigma'], dims=['equations'])
-                pymc.Normal(f'likelihood_{group}', mu=mean, sigma=sigma, observed=segment.values[n_lags:])
-
-            if prior_checks:
-                idata = pymc.sample_prior_predictive(random_seed=self.__configurations.seed)
-                return model, idata
-            else:
-                idata = pymc.sample_prior_predictive(random_seed=self.__configurations.seed)
-                idata.extend(pymc.sampling.jax.sample_blackjax_nuts(
-                    draws=2000, random_seed=self.__configurations.seed, chains=4, target_accept=0.95))
-                idata.extend(pymc.sample_posterior_predictive(idata))
-
-        return model, idata
+            pass
