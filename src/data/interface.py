f967b811ec3ed36881118fa7da5ec1d096069251
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-24 21:34:33 +0000

    Update src/data/interface.py: In Progress

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,5 +1,4 @@
 """Module interface.py"""
-import logging
 
 import pandas as pd
 
@@ -29,12 +28,6 @@ class Interface:
         # Instances
         self.__streams = src.functions.streams.Streams()
         self.__configurations = config.Config()
-
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
 
     def __get_data(self, uri: str) -> pd.DataFrame:
         """
