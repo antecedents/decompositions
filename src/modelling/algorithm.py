3bf463eba0ef273f75576ee3aa2000ab8c24524b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-25 21:20:08 +0000

    Update src/modelling/algorithm.py

--- src/modelling/algorithm.py
+++ src/modelling/algorithm.py
@@ -1,13 +1,12 @@
 """Module algorithm.py"""
 import typing
 
-import numpy as np
+import arviz
 import pandas as pd
 import pymc
 import pymc.sampling.jax
 
 import config
-import src.modelling.marginals
 
 
 class Algorithm:
@@ -15,32 +14,57 @@ class Algorithm:
     The multi-level algorithm
     """
 
-    def __init__(self) -> None:
+    def __init__(self, arguments: dict) -> None:
         """
         Constructor
         """
 
-        self.__configurations = config.Config()
-        self.__marginals = src.modelling.marginals.Marginals()
+        self.__priors = arguments['priors']
 
-    def exc(self, n_lags: int, n_equations: int, data: pd.DataFrame, leaves: str, prior_checks: bool = False):
+        self.__configurations = config.Config()
+
+    def exc(self, data: pd.DataFrame) -> typing.Tuple[pymc.model.core.Model, arviz.data.InferenceData]:
         """
 
-        :param n_lags: <br># of non-constant coefficients<br>
-        :param n_equations: <br># of independent variables.  Beware, this algorithm is inappropriate for cases whereby
-                      there are two or more variates because it does not consider multi-variate covariance.<br>
         :param data: <br>The training data of a board; a board consists of one or more hospitals/institutions.<br>
-        :param leaves: <br>The field that identifies an instance's group<br>
-        :param prior_checks: <br>Focusing on priors only?<br>
         :return:
         """
 
-        cols = [col for col in data.columns if col != leaves]
-        coords = {'lags': np.arange(n_lags) + 1,
-                  'equations': cols}
+        timings = list(range(data.shape[0]))
 
-        groups = data[leaves].unique()
+        coords = {'obs_id': timings}
 
         with pymc.Model(coords=coords) as model:
 
-            pass
+            # The data containers
+            points = pymc.Data('points', timings, dims='obs_id')
+            observations = pymc.Data('observations', data['dt'].to_numpy(), dims='obs_id')
+
+            # Setting priors for each coefficient in the AR process
+            coefficients = pymc.Normal(
+                'coefficients', mu=self.__priors['coefficients']['mu'],
+                sigma=self.__priors['coefficients']['sigma'], size=self.__priors['coefficients']['size'])
+            sigma = pymc.HalfNormal('sigma', self.__priors['sigma'])
+            degree = pymc.Uniform('degree', lower=self.__priors['degree']['lower'], upper=self.__priors['degree']['upper'])
+
+            # Initialisation per ...
+            init = pymc.Normal.dist(
+                self.__priors['init']['mu'], self.__priors['init']['sigma'], size=self.__priors['init']['size']
+            )
+
+            # Autoregressive process with p lags; lags = self.__priors['coefficients']['size'] - 1
+            process = pymc.AR(
+                'ar', coefficients, sigma=sigma, init_dist=init, constant=True,
+                steps=points.eval().shape[0] - (self.__priors['coefficients']['size'] - 1),
+                dims='obs_id')
+
+            # Likelihood
+            pymc.StudentT('likelihood', mu=process, sigma=sigma, nu=degree, observed=observations, dims='obs_id')
+
+            # Sampling
+            details = pymc.sample_prior_predictive()
+
+            details.extend(pymc.sample(3000, random_seed=100, target_accept=0.95))
+            details.extend(pymc.sample_posterior_predictive(details))
+
+        return model, details
