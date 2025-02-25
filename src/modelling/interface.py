53c09e4b9b2232f1e626cccb792a31ca8f7872dd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-25 15:56:06 +0000

    Update src/modelling/interface.py: In Progress

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -1,11 +1,11 @@
 """Module interface.py"""
 import logging
-import boto3
 
+import boto3
 import pandas as pd
 
-import config
 import src.modelling.splits
+import src.s3.configurations
 
 
 class Interface:
@@ -22,8 +22,15 @@ class Interface:
         self.__connector = connector
         self.__stamp = stamp
 
-        # Configurations, etc
-        self.__configurations = config.Config()
+    def __arguments(self) -> dict:
+        """
+
+        :return:
+        """
+
+        return src.s3.configurations.Configurations(connector=self.__connector).objects(
+            key_name=('architecture' + 'single' + '/' + 'difference' + 'arguments.json')
+        )
 
     def exc(self, data: pd.DataFrame):
         """
@@ -31,7 +38,7 @@ class Interface:
         :return:
         """
 
+        arguments = self.__arguments()
+
         # Splits
         training, testing = src.modelling.splits.Splits(data=data).exc()
-
-
