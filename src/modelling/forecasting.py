6f16801ff4885387450244b741fdce94c453bc2f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-26 22:08:20 +0000

    Update src/modelling/forecasting.py

--- src/modelling/forecasting.py
+++ src/modelling/forecasting.py
@@ -1,6 +1,16 @@
-
+import pymc
 
 class Forecasting:
 
-    def __init__(self):
-        pass
+    def __init__(self, arguments: dict):
+
+        self.__arguments = arguments
+
+    def exc(self, model: pymc.model.core.Model, n_instances: int):
+
+        starting = n_instances - self.__arguments['n_lags']
+        ending = n_instances + self.__arguments['ahead']
+
+        with model:
+            model.add_coords({'id_estimating': range(starting, ending, 1)})
+            model.add_coords({'id_forecasting': range(n_instances, ending, 1)})
