fc7a8f7976b2da9eb293d45feecc69b63ccfd420
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-26 22:51:04 +0000

    Update src/modelling/inverting.py

--- src/modelling/inverting.py
+++ src/modelling/inverting.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Inverting:
 
@@ -12,3 +13,9 @@ class Inverting:
         return self.__master.index.append(
             pd.date_range(start=self.__master.index.max(), periods=self.__arguments['ahead'] + 1,
                           freq='W', inclusive='right'))
+
+    def __invert(self, values: np.ndarray):
+
+        # Append estimates
+        starting: np.ndarray = self.__master['dt'].to_numpy()
+        points = np.concatenate((starting, values))
