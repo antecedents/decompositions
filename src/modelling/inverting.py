a6eaa538e048ec750b5b41217735ed17f90fb534
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-26 22:43:19 +0000

    Update src/modelling/inverting.py

--- src/modelling/inverting.py
+++ src/modelling/inverting.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 class Inverting:
 
-    def __init__(self, master: pd.DataFrame):
+    def __init__(self, master: pd.DataFrame, arguments: dict):
 
         self.__master = master
+        self.__arguments = arguments
+
+    def __times(self):
+        pass
