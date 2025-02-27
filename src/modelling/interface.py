72790d048f9da667baba7eef4673416e06a9b863
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-27 17:10:26 +0000

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -4,6 +4,7 @@ import boto3
 import pandas as pd
 
 import src.modelling.algorithm
+import src.modelling.forecasting
 
 
 class Interface:
@@ -38,8 +39,10 @@ class Interface:
     def exc(self):
 
         algorithm = src.modelling.algorithm.Algorithm(arguments=self.__arguments)
+        forecasting = src.modelling.forecasting.Forecasting(arguments=self.__arguments)
 
         for code in self.__training['hospital_code'].unique():
 
             data = self.__get_data(code=code)
             model, details = algorithm.exc(data=data)
+            predictions = forecasting.exc(model=model, details=details)
