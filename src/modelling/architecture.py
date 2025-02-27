bdde6fae1a6546c4ec897682c438d3b04538b447
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-27 14:35:58 +0000

    Update src/modelling/architecture.py

--- src/modelling/architecture.py
+++ src/modelling/architecture.py
@@ -19,16 +19,15 @@ class Architecture:
         # Configurations
         self.__configurations = config.Config()
 
-    def exc(self, model: pymc.model.core.Model, stamp: str, name: str):
+    def exc(self, model: pymc.model.core.Model, hospital_code: str):
         """
 
         :param model: The estimated model
-        :param stamp:
-        :param name:
+        :param hospital_code:
         :return:
         """
 
         pathstr = os.path.join(
-            self.__configurations.artefacts_, stamp, 'architecture', f'{name}.pdf')
+            self.__configurations.artefacts_models, hospital_code, 'architecture.pdf')
 
         pymc.model_to_graphviz(model, save=pathstr, figsize=(5,5))
