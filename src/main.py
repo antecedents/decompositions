534c1449f69fd1ff0aff80cde1f38a00a550d33d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-12 22:30:16 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ import os
 import sys
 
 import boto3
+import jax
+import numpyro
+import pytensor
 
 
 def main():
@@ -13,21 +16,23 @@ def main():
     """
 
     logger: logging.Logger = logging.getLogger(__name__)
-    logger.info(arguments)
+    logger.info('BLAS: %s', pytensor.config.blas__ldflags)
 
     # Setting up
     src.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
 
-    # Steps
-    data, training, testing = src.data.interface.Interface(
-        s3_parameters=s3_parameters, arguments=arguments).exc()
-    data.info()
-    training.info()
-    testing.info()
+    # Data
+    data = src.data.interface.Interface(s3_parameters=s3_parameters).exc()
 
-    '''
-    Cache
-    '''
+    # Modelling
+    src.modelling.interface.Interface(
+      data=data, arguments=arguments).exc()
+
+    # Transfer
+    src.transfer.interface.Interface(
+       connector=connector, service=service, s3_parameters=s3_parameters).exc()
+
+    # Cache
     src.functions.cache.Cache().exc()
 
 
@@ -43,21 +48,32 @@ if __name__ == '__main__':
                         datefmt='%Y-%m-%d %H:%M:%S')
 
     # Classes
+    import environment
     import src.data.interface
     import src.functions.cache
     import src.functions.service
     import src.modelling.interface
-    import src.s3.configurations
     import src.s3.s3_parameters
+    import src.s3.configurations
     import src.setup
+    import src.transfer.interface
 
-    # Amazon: Connector, S3 Parameters, Service
+    # Vis-à-vis Amazon & Development: Connector, S3 Parameters, Platform Services, Configurations
     connector = boto3.session.Session()
     s3_parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
     service = src.functions.service.Service(connector=connector, region_name=s3_parameters.region_name).exc()
+    arguments: dict = src.s3.configurations.Configurations(connector=connector).objects(
+        key_name=('artefacts' + '/' + 'architecture' + '/' + 'single' + '/' + 'futures' + '/' + 'arguments.json'))
 
-    # Modelling arguments
-    arguments = src.s3.configurations.Configurations(connector=connector).objects(
-        key_name=('architecture' + '/' + 'single' + '/' + 'difference' + '/' + 'arguments.json'))
+    pytensor.config.blas__ldflags = '-llapack -lblas -lcblas'
+
+    jax.config.update('jax_platform_name', 'cpu')
+    jax.config.update('jax_enable_x64', True)
+
+    numpyro.set_platform('cpu')
+    numpyro.set_host_device_count(jax.device_count(backend='cpu'))
+
+    # Environment Variables
+    environment.Environment(arguments=arguments)
 
     main()
