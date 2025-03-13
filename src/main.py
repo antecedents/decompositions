2a79df1987278461ff39a505e4c8c7caa9a7d752
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-13 19:26:15 +0000

    Update src/main.py: Re-writing

--- src/main.py
+++ src/main.py
@@ -3,11 +3,6 @@ import logging
 import os
 import sys
 
-import boto3
-import jax
-import numpyro
-import pytensor
-
 
 def main():
     """
@@ -36,11 +31,6 @@ if __name__ == '__main__':
     root = os.getcwd()
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
-
-    # Logging
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                        datefmt='%Y-%m-%d %H:%M:%S')
 
     # Classes
     import src.data.interface
