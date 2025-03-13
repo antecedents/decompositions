b9e25ce8ecabff4f8ba5b94c221816a5f267dfc1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-13 17:37:08 +0000

    Update src/script.py

--- src/script.py
+++ src/script.py
@@ -2,14 +2,16 @@ import logging
 import multiprocessing
 
 def computations():
-    logging.info(multiprocessing.cpu_count())
+    query = multiprocessing.current_process()
+    logging.info('Current Process: %s', query.pid)
+    logging.info('# of cores: %s', multiprocessing.cpu_count())
 
 
 if __name__ == '__main__':
 
     # Logging
     logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n\n',
+                        format='\n%(message)s\n%(asctime)s.%(msecs)03d\n',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
     # .join() ensures script waits for <process> to terminate
