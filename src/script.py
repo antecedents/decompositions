39ebc0d27293dd0e35a9fc6587a25b99089e22d5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-13 17:27:27 +0000

    Add src/script.py

---
+++ src/script.py
@@ -0,0 +1,16 @@
+import logging
+import multiprocessing
+
+def computations():
+    logging.info(multiprocessing.cpu_count())
+
+
+if __name__ == '__main__':
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n\n',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    process = multiprocessing.Process(target=computations)
+    process.start()
