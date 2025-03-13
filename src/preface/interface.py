3f29fdd5946f62d54027e8451748d9c91528db0e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-13 19:35:00 +0000

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -1,27 +1,41 @@
 import logging
 import os
-import boto3
-import pytensor
-import numpyro
-import jax
 import typing
 
+import boto3
+import jax
+import numpyro
+import pytensor
 
+import src.data.interface
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
-import src.preface.environment
-import src.data.interface
 import src.functions.cache
 import src.functions.service
 import src.modelling.interface
-import src.s3.s3_parameters
+import src.preface.environment
 import src.s3.configurations
+import src.s3.s3_parameters
 import src.setup
 import src.transfer.interface
 
+
 class Interface:
+    """
+    Interface
+    """
 
     def __init__(self):
+        """
+
+        """
+
+        self.__connector = boto3.session.Session()
+        self.__s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=self.__connector).exc()
+        self.__service: sr.Service = src.functions.service.Service(
+            connector=self.__connector, region_name=self.__s3_parameters.region_name).exc()
+
+        pytensor.config.blas__ldflags = '-llapack -lblas -lcblas'
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -30,8 +44,23 @@ class Interface:
 
         self.__logger: logging.Logger = logging.getLogger(__name__)
 
+    def __get_arguments(self) -> dict:
+        """
+
+        :return:
+        """
+
+        key_name = 'artefacts' + '/' + 'architecture' + '/' + 'single' + '/' + 'futures' + '/' + 'arguments.json'
+
+        return src.s3.configurations.Configurations(connector=self.__connector).objects(key_name=key_name)
+
     @staticmethod
     def __compute(arguments: dict):
+        """
+
+        :param arguments:
+        :return:
+        """
 
         jax.config.update('jax_platform_name', 'cpu')
         jax.config.update('jax_enable_x64', True)
@@ -50,7 +79,6 @@ class Interface:
         :return:
         """
 
-        # Setting up
         src.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
 
     def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
@@ -59,17 +87,9 @@ class Interface:
         :return:
         """
 
-        connector = boto3.session.Session()
-        s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
-        service: sr.Service = src.functions.service.Service(connector=connector, region_name=s3_parameters.region_name).exc()
-
-        arguments: dict = src.s3.configurations.Configurations(connector=connector).objects(
-            key_name=('artefacts' + '/' + 'architecture' + '/' + 'single' + '/' + 'futures' + '/' + 'arguments.json'))
-
-        pytensor.config.blas__ldflags = '-llapack -lblas -lcblas'
+        arguments: dict = self.__get_arguments()
         self.__compute(arguments=arguments)
-        self.__setting_up(service=service, s3_parameters=s3_parameters)
-
+        self.__setting_up(service=self.__service, s3_parameters=self.__s3_parameters)
         self.__logger.info('BLAS: %s', pytensor.config.blas__ldflags)
 
-        return connector, s3_parameters, service, arguments
+        return self.__connector, self.__s3_parameters, self.__service, arguments
