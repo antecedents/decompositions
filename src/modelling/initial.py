3b9433d56d22eec9ac22d717619dcdbb69b80fec
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-14 12:19:57 +0000

    Update src/modelling/initial.py

--- src/modelling/initial.py
+++ src/modelling/initial.py
@@ -1,6 +1,7 @@
 """Module initial.py"""
 import dask
 import pandas as pd
+import numpy as np
 
 import src.elements.codes as ce
 import src.elements.master as mr
@@ -38,6 +39,7 @@ class Initial:
         frame = self.__data.copy().loc[self.__data['hospital_code'] == code.hospital_code, :]
         frame.set_index(keys='week_ending_date', drop=True, inplace=True)
         frame.sort_values(by=['week_ending_date'], ascending=True, ignore_index=False, inplace=True)
+        frame['ln'] = np.log(frame['n_attendances'])
 
         return frame
 
@@ -60,9 +62,9 @@ class Initial:
         for code in self.__codes:
 
             data: pd.DataFrame = self.__get_data(code=code)
-            decompositions: pd.DataFrame = decompose(data=data)
-            master: mr.Master = splits(data=decompositions, code=code)
-            master_: mr.Master | None = sc(master=master, code=code)
+            master: mr.Master = splits(data=data, code=code)
+            _master: mr.Master = decompose(master=master, code=code)
+            master_: mr.Master | None = sc(master=_master, code=code)
             computations.append(master_)
 
         masters = dask.compute(computations, scheduler='threads')[0]
