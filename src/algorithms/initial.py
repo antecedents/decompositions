151bf8e5ca1f6c73e4657b81cb3c6a348768c147
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-02 17:16:59 +0100

    Update src/algorithms/initial.py

--- src/algorithms/initial.py
+++ src/algorithms/initial.py
@@ -2,9 +2,10 @@
 import dask
 import pandas as pd
 
-import src.elements.codes as ce
-import src.functions.directories
 import src.algorithms.decompose
+import src.algorithms.persist
+import src.elements.specifications as se
+import src.functions.directories
 
 
 class Initial:
@@ -12,27 +13,27 @@ class Initial:
     Seasonal component modelling.
     """
 
-    def __init__(self, data: pd.DataFrame, codes: list[ce.Codes], arguments: dict):
+    def __init__(self, data: pd.DataFrame, specifications_: list[se.Specifications], arguments: dict):
         """
 
         :param data: The weekly accidents & emergency data of institutions/hospitals
-        :param codes: The unique set of health board & institution pairings.
+        :param specifications_: The unique set of health board & institution pairings.
         :param arguments: A set of model development, and supplementary, arguments.
         """
 
         self.__data = data
-        self.__codes = codes
+        self.__specifications_ = specifications_
         self.__arguments = arguments
 
     @dask.delayed
-    def __get_data(self, code: ce.Codes) -> pd.DataFrame:
+    def __get_data(self, specifications: se.Specifications) -> pd.DataFrame:
         """
 
-        :param code: The health board & institution/hospital codes of an institution/hospital.
+        :param specifications: The board & institution codes, etc.
         :return:
         """
 
-        frame = self.__data.copy().loc[self.__data['hospital_code'] == code.hospital_code, :]
+        frame = self.__data.copy().loc[self.__data['hospital_code'] == specifications.hospital_code, :]
         frame.set_index(keys='week_ending_date', drop=True, inplace=True)
         frame.sort_values(by=['week_ending_date'], ascending=True, ignore_index=False, inplace=True)
 
@@ -45,12 +46,14 @@ class Initial:
         """
 
         # Additional delayed tasks
-        decompose = dask.delayed(src.algorithms.decompose.Decompose(arguments=self.__arguments).exc)
+        __decompose = dask.delayed(src.algorithms.decompose.Decompose(arguments=self.__arguments).exc)
+        __persist = dask.delayed(src.algorithms.persist.Persist().exc)
 
         computations = []
-        for code in self.__codes:
-            data: pd.DataFrame = self.__get_data(code=code)
-            message = decompose(data=data, code=code)
+        for specifications in self.__specifications_:
+            data: pd.DataFrame = self.__get_data(specifications=specifications)
+            decompositions = __decompose(data=data)
+            message = __persist(decompositions=decompositions, specifications=specifications)
             computations.append(message)
         messages = dask.compute(computations, scheduler='threads')[0]
 
