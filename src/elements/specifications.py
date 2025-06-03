371ec6eef78e8dfb6540c38a8d603733d39d0eee
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-03 15:21:11 +0100

    Update src/elements/specifications.py

--- src/elements/specifications.py
+++ src/elements/specifications.py
@@ -23,13 +23,16 @@ class Specifications(typing.NamedTuple):
         The codes of <a href="https://hscscotland.scot/hscps/" target="_blank">Health and Social Care Partnerships</a><br>
     council_area: str
         The <a href="https://www.mygov.scot/organisations#scottish-local-authority" target="_blank">council area</a> locale
-        of the hospital/institution; <a href="https://geoportal.statistics.gov.uk/documents/7ef19fb100de4c1ab964f65599e9534b/explore"
+        of the hospital/institution;
+        <a href="https://geoportal.statistics.gov.uk/documents/7ef19fb100de4c1ab964f65599e9534b/explore"
         target="_blank">map</a>.<br>
     intermediate_zone: str
-        <a href="https://geoportal.statistics.gov.uk/datasets/ons::intermediate-zones-december-2001-names-and-codes-in-sc/about"
+        <a
+        href="https://geoportal.statistics.gov.uk/datasets/ons::intermediate-zones-december-2001-names-and-codes-in-sc/about"
         target="_blank">Intermediate Zone</a><br>
     data_zone: str
-        <a href="https://geoportal.statistics.gov.uk/datasets/ons::data-zones-december-2011-names-and-codes-in-sc/about">Data Zones</a>
+        <a href="https://geoportal.statistics.gov.uk/datasets/ons::data-zones-december-2011-names-and-codes-in-sc/about">
+        Data Zones</a>
     """
 
     hospital_code: str
